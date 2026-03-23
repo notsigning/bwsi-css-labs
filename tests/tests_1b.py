c7547916dd0b01c9d3235decfc2030144af091e1
notsigning <wenzhu317@gmail.com>
2026-03-22 21:23:46 -0700

    changed test

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -28,7 +28,7 @@ def test_division():
     assert simple_calculator("divide", 5, 2) == 2.5     # Test for division resulting in float
 
 def test_division_by_zero():
-    with pytest.raises(ValueError, match="Cannot divide by zero."):
+    with pytest.raises(ZeroDivisionError,match="Please select a different operation/second value"):
         simple_calculator("divide", 5, 0)               # Test division by zero
 
 def test_invalid_operation():
